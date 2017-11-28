62773e2c27ad5189c419e7a8364a41d366dffb34
Alex Liang <unpiraticalturtle@gmail.com>
2017-11-28 01:54:53 -0500

    some merge issues and also ids are auto incrementing

--- Vapor/create/views.py
+++ Vapor/create/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import NewProductForm
-from data.models import Products
+from data.models import Product
 
 # Create your views here.
 
@@ -10,7 +10,7 @@ def product_view(request):
 		if form.is_valid():
 			cform = form.cleaned_data
 			print(cform)
-			new_product = Products(product_name=cform['product_name'], product_description=cform['product_desc'], price=cform['price'])
+			new_product = Product(product_name=cform['product_name'], product_description=cform['product_desc'], price=cform['price'])
 			new_product.save()
 	else:
 		form = NewProductForm()
