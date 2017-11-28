6a8b0b1757a2d3c79e7cc09b6f3d0bf0840237a1
Alex <unpiraticalturtle@gmail.com>
2017-11-28 14:39:26 -0500

    bugfix with homepage involving not being logged in

--- Vapor/home/views.py
+++ Vapor/home/views.py
@@ -3,6 +3,7 @@ from data.models import Customer as User
 
 
 def homepage(request):
+	context = {'is_logged_in': False}
 	if request.user.is_authenticated():
 		context = {'is_logged_in': True}
 	return render(request, 'home/home.html', context)
