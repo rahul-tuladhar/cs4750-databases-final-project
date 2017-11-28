2f22f1a89d590a20a74ad40984472392d617611e
Alex Liang <unpiraticalturtle@gmail.com>
2017-11-28 11:47:45 -0500

    new product page

--- Vapor/create/forms.py
+++ Vapor/create/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from data.models import Merchant, Developer
 
 class NewProductForm(forms.Form):
 	product_name = forms.CharField(label="Product Name", widget=forms.TextInput(attrs={'class':'form-control'}))
 	product_desc = forms.CharField(label="Product Description", widget=forms.TextInput(attrs={'class':'form-control'}))
 	price = forms.CharField(label="Price", widget=forms.TextInput(attrs={'class':'form-control'}))
+	genre = forms.CharField(label="Genre", widget=forms.TextInput(attrs={'class':'form-control'}))
+	release_date = forms.DateTimeField(label="Release Date")
+	stock = forms.IntegerField(label="In Stock")
+	merchant_id = forms.ModelChoiceField(queryset=Merchant.objects.all(), empty_label="Merchant")
+	developer_id = forms.ModelChoiceField(queryset=Developer.objects.all(), empty_label="Developer")
+	
